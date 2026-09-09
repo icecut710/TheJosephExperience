1d387d119f8f99bf27d5b228815cbfb9acf5a45c
icecut710 <dev@icecut710.com>
2026-09-09 16:26:53 -0400

    v2.1.4: update dialog fix + chart polish restore, release manifest

--- GoodJobJoseph/AssemblyInfo.cs
+++ GoodJobJoseph/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Windows;
 [assembly: AssemblyProduct("The Joseph Experience")]
 [assembly: AssemblyCompany("The Joseph Experience")]
 [assembly: AssemblyDescription("Good Job, Joseph!")]
-[assembly: AssemblyVersion("2.1.3.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
-[assembly: AssemblyInformationalVersion("2.1.3")]
+[assembly: AssemblyVersion("2.1.4.0")]
+[assembly: AssemblyFileVersion("2.1.4.0")]
+[assembly: AssemblyInformationalVersion("2.1.4")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
